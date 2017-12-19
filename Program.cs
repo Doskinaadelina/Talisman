944a9cfd548259695e777d72994e29c0bc9e9ccc
Doskinaadelina <34675409+Doskinaadelina@users.noreply.github.com>
2017-12-19 14:55:02 +0600

    deleted comments

--- Program.cs
+++ Program.cs
@@ -8,9 +8,7 @@ namespace Minesweeper
 {
     static class Program
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
+        
         [STAThread]
         static void Main()
         {
